3821b34db719ceb55cc493edb3f716413c265b4c
Chernyshov_Nikita <xpom5566@mail.ru>
2020-03-24 21:42:56 +0300

    save point, idea in comments

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
+from typing import List
+
+
+class Solution:
+
+  def permute(self, nums: List[int]) -> List[List[int]]:
+    ans = []
+
+    count = 0
+    take = nums[0]
+    other = nums[1:]
+
+    while count < len(nums):
+      count += 1
+      # generate ans as below
+
+    return ans
+
+
 my = Solution()
-v = 0
-ans = my.(v)
-print("ans", ans)
+n = [1, 2, 3]
+ans = my.permute(n)
+nAns = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+print("ans", ans, ans=nAns)
+
+# 1, 2, 3
+
+# 0, 1, 2
+
+# 0, 1, 2
+# 0 = [1, 2]
+# 0, 1
+# 0, 2
+# 1 = [0, 2]
+# 1, 0
+# 1, 2
+# 2 = [1, 3]
+# 2, 0
+# 2, 1
